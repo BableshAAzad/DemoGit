d76dd3a2ae51d30d0dd1b4d2b024bbc2d904929b
bableshaazad <bableshaazad@outlook.com>
2024-05-29 17:16:32 +0530

    branch2 second commit

--- src/com/DemoClass.java
+++ src/com/DemoClass.java
@@ -3,5 +3,7 @@ package com;
 public class DemoClass {
 	public static void main(String[] args) {
 		System.out.println("Main first commit");
+		
+		System.out.println("branch2 first commit");
 	}
 }
